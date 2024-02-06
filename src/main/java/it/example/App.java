8a844a83504f5c67cb29b0e56e85b284ca9de2b3
alessio176 <alessio176@gmail.com>
2024-02-06 23:07:36 +0100

    Fix logger + add sample code in main

--- src/main/java/it/example/App.java
+++ src/main/java/it/example/App.java
@@ -1,5 +1,8 @@
 package it.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        List<Float> a = new ArrayList<>();
+        a.add(2.0F);
+        a.add(1.0F);
+        MeanUtils.computeMean(a);
+
     }
 }
